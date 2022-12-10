26c723e68e66a3f2768e391368a822b3ceded68e
Salal20 <rubinaburiro@yahoo.com>
2022-12-09 19:45:14 -0500

    first commit

---
+++ src/test/java/maven_project_2/Automation_Test.java
@@ -0,0 +1,22 @@
+package maven_project_2;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Automation_Test {
+
+	public static void main(String[] args) {
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://amazone.com");
+		
+		
+
+
+
+	}
+
+}
